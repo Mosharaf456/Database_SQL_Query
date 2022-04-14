01eb6d0d7f7e94caff133c57108ae096b541f17c
Mosharaf456 <mosharaf111hossain@gmail.com>
2022-04-14 15:00:27 +0600

    session variable into call function @variable of Stored Procedure

--- 4_out_call_SP.sql
+++ 4_out_call_SP.sql
@@ -1,2 +1,7 @@
+/*seesion variable is a user defined variable as a parameter pass,,, 
+so don,t require declartion of that session variable  @ sine variable
+non visible for other session
+and exists the end of current session
+*/
 CALL select_avg_money(@avg_money);
 select @avg_money as average_money;
